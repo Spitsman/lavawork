8636d1cf6f2eb4e425918f001bfaa61d58c924e0
Alexander Spitsyn <sanspits@yandex.ru>
2018-01-29 15:41:41 +0300

    notify users when lave send

--- app/services/accrual_service.rb
+++ app/services/accrual_service.rb
@@ -2,8 +2,14 @@ class AccrualService
 
   def self.call
     Resident.all.each do |resident|
-      resident.amount = resident.current_amount + Settings.additional_amount.to_f * resident.rating
+      additional_amount = Settings.additional_amount.to_f * resident.rating
+      resident.amount = resident.current_amount + additional_amount
       resident.save
+
+      Telegram.bot.send_message(
+        chat_id: resident.telegram_id,
+        text: "Тебе начислено #{additional_amount}l (по #{Settings.additional_amount}l за каждый балл рейтинга)"
+      ) if additional_amount > 0
     end
   end
 
